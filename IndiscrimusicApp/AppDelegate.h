6e0677dcc3b5e73c48ec5003ede65124f7354673
Dalton <fanboyfanboydev@hotmail.com>
2017-02-21 17:47:00 -0600

    Initial Commit

---
+++ IndiscrimusicApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  IndiscrimusicApp
+//
+//  Created by Dalton Cothron on 2/21/17.
+//  Copyright © 2017 Indiscrimusic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ IndiscrimusicApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndiscrimusicApp
+//
+//  Created by Dalton Cothron on 2/21/17.
+//  Copyright © 2017 Indiscrimusic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
